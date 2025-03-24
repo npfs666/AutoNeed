20052154ce1ceadac68dd76c4be9d615196c552a
gaou <brendan.jaouen@ik.me>
2025-03-24 18:10:45 +0100

    correction

--- AutoNeed.lua
+++ AutoNeed.lua
@@ -1,10 +1,10 @@
-DEFAULT_CHAT_FRAME:AddMessage("Testing, testing", 1, 0, 1);
+DEFAULT_CHAT_FRAME:AddMessage("AutoNeed loaded", 1, 0, 1);
 BMLoot = CreateFrame("Frame")
 BMLoot:RegisterEvent("START_LOOT_ROLL") 
 BMLoot:RegisterEvent("LOOT_BIND_CONFIRM")
 BMLoot:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 BMLoot:SetScript("OnEvent", function()
-    if event == "START_LOOT_ROLL" then
+    if (UnitIsDeadOrGhost("player") == nil ) and (event == "START_LOOT_ROLL") then
         local rollID, rollTime, rollItemLink, rollQuantity, rollPlayer = arg1, arg2, arg3, arg4, arg5
 		local texture, name, count, quality = GetLootRollItemInfo(rollID)
 		
